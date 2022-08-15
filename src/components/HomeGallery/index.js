596b020b01b359b2542b262adeb3412aed0bdc82
Alexandru Gorgos <hello@alexandrugorgos.com>
2022-08-16 00:28:14 +0100

    debugging

--- src/components/HomeGallery/index.js
+++ src/components/HomeGallery/index.js
@@ -1,34 +1,45 @@
 import * as React from "react";
-import { Swiper, SwiperSlide } from "swiper/react";
-import { Keyboard, Mousewheel, Virtual } from "swiper";
+import Swiper, { Keyboard, Mousewheel, Virtual } from "swiper";
 import { GatsbyImage, getImage } from "gatsby-plugin-image";
 
 import "swiper/css";
-import "swiper/css/navigation";
 
 import "./styles.css";
+import { Box } from "@mui/material";
+import { useEffect } from "react";
 
 export const HomeGallery = ({ gallery }) => {
+  useEffect(() => {
+    const swiper = new Swiper(".swiper", {
+      modules: [Keyboard, Mousewheel],
+      spaceBetween: 30,
+      centeredSlides: true,
+      slidesPerView: 1.5,
+      mousewheel: true,
+      keyboard: true,
+      grabCursor: true,
+      on: {
+        init: () => {
+          console.log("swiper initialized");
+        },
+      },
+    });
+  }, []);
+
   return (
-    <Swiper
-      initialSlide={0}
-      modules={[Keyboard, Mousewheel, Virtual]}
-      centeredSlides={true}
-      slidesPerView={"auto"}
-      spaceBetween={30}
-      mousewheel
-      keyboard
-      virtual={typeof window !== "undefined" ? false : true}
-      grabCursor
-    >
-      {gallery.featuredImages.map((image, i) => {
-        const gimage = getImage(image.localFile);
-        return (
-          <SwiperSlide key={i} virtualIndex={i}>
-            <GatsbyImage image={gimage} alt={image.alternativeText} />
-          </SwiperSlide>
-        );
-      })}
-    </Swiper>
+    <Box className="swiper">
+      <Box className="swiper-wrapper">
+        {gallery.featuredImages.map((image) => {
+          return (
+            <Box className="swiper-slide">
+              <GatsbyImage
+                image={getImage(image.localFile)}
+                alt={image.alternativeText}
+              />
+            </Box>
+          );
+        })}
+      </Box>
+    </Box>
   );
 };
