1e93f744ac5dcee05a7264fc7fac202f038db8c7
Alexandru Gorgos <hello@alexandrugorgos.com>
2022-08-16 16:51:33 +0100

    debugging

--- src/components/HomeGallery/index.js
+++ src/components/HomeGallery/index.js
@@ -1,37 +1,121 @@
 import * as React from "react";
-import Swiper, { Keyboard, Mousewheel, Virtual } from "swiper";
+import Swiper, { Keyboard, Mousewheel } from "swiper";
 import { GatsbyImage, getImage } from "gatsby-plugin-image";
+import SwipeIcon from "@mui/icons-material/Swipe";
+import KeyboardIcon from "@mui/icons-material/Keyboard";
+import PanToolIcon from "@mui/icons-material/PanTool";
+import MouseIcon from "@mui/icons-material/Mouse";
+import KeyboardArrowLeftIcon from "@mui/icons-material/KeyboardArrowLeft";
+import KeyboardArrowRightIcon from "@mui/icons-material/KeyboardArrowRight";
 
 import "swiper/css";
 
 import "./styles.css";
-import { Box } from "@mui/material";
-import { useEffect } from "react";
+import { Box, Modal, Typography } from "@mui/material";
+
+const modal = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  p: 4,
+  color: "white",
+  textAlign: "center",
+  outline: 0,
+};
 
 export const HomeGallery = ({ gallery }) => {
-  useEffect(() => {
+  const [helper, setHelper] = React.useState(null);
+
+  const isMobile = React.useRef(
+    (() => {
+      const toMatch = [
+        /Android/i,
+        /webOS/i,
+        /iPhone/i,
+        /iPad/i,
+        /iPod/i,
+        /BlackBerry/i,
+        /Windows Phone/i,
+      ];
+
+      return toMatch.some((toMatchItem) => {
+        return navigator.userAgent.match(toMatchItem);
+      });
+    })()
+  );
+
+  React.useEffect(() => {
     const swiper = new Swiper(".swiper", {
       modules: [Keyboard, Mousewheel],
       spaceBetween: 30,
       centeredSlides: true,
-      slidesPerView: 1.5,
+      slidesPerView: 1,
       mousewheel: true,
       keyboard: true,
       grabCursor: true,
-      on: {
-        init: () => {
-          console.log("swiper initialized");
+      breakpoints: {
+        900: {
+          slidesPerView: 2,
         },
       },
+    });
+
+    swiper.on(
+      "afterInit",
+      setTimeout(() => {
+        helper == null && setHelper(true);
+      }, 10 * 1000)
+    );
+
+    swiper.on("slideChange", () => {
+      helper && setHelper(false);
+    });
+
+    swiper.on("activeIndexChange", () => {
+      helper && setHelper(false);
+    });
+
+    swiper.on("touchMove", () => {
+      helper && setHelper(false);
     });
   }, []);
 
   return (
     <Box className="swiper">
+      <Modal open={helper != null && helper} onClose={() => setHelper(false)}>
+        <Box sx={modal}>
+          {isMobile.current ? (
+            <>
+              <Box className="wiggle">
+                <KeyboardArrowLeftIcon sx={{ fontSize: 60 }} />
+                <SwipeIcon sx={{ fontSize: 60 }} />
+                <KeyboardArrowRightIcon sx={{ fontSize: 60 }} />
+              </Box>
+              <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+                Swipe
+              </Typography>
+            </>
+          ) : (
+            <>
+              <Box className="wiggle">
+                <KeyboardArrowLeftIcon sx={{ fontSize: 60 }} />
+                <KeyboardIcon sx={{ fontSize: 60 }} />
+                <MouseIcon sx={{ fontSize: 60 }} />
+                <PanToolIcon sx={{ fontSize: 60 }} />
+                <KeyboardArrowRightIcon sx={{ fontSize: 60 }} />
+              </Box>
+              <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+                Use keyboard, mousewheel or hold left-click to scroll
+              </Typography>
+            </>
+          )}
+        </Box>
+      </Modal>
       <Box className="swiper-wrapper">
-        {gallery.featuredImages.map((image) => {
+        {gallery.featuredImages.map((image, i) => {
           return (
-            <Box className="swiper-slide">
+            <Box key={i} className="swiper-slide">
               <GatsbyImage
                 image={getImage(image.localFile)}
                 alt={image.alternativeText}
